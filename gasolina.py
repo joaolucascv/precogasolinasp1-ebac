672cfd07538223323a8b7ad19fb5859c35a4cc06
joaolucascv <joaolucascv@gmail.com>
2024-09-04 20:00:11 +0000

    arquivos gasolina.csv, gasolina.png, gasolina.py criandos em dev

--- gasolina.py
+++ gasolina.py
@@ -7,4 +7,5 @@ gasolina_df = pd.read_csv('gasolina.csv', sep=',')
 with sns.axes_style('whitegrid'):
 
   grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda", palette="pastel")
+  grafico.set(title='Preço médio de venda da gasolina por dia', xlabel='Dia (Julho de 2021)', ylabel='Preço (R$)');
   plt.savefig('gasolina.png')
